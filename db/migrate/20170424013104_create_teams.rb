27ae5ccd68ccddd98438d897742465961ae94ac2
btcline04 <btcline04@gmail.com>
2017-04-23 21:42:44 -0400

    added migrations and tables

---
+++ db/migrate/20170424013104_create_teams.rb
@@ -0,0 +1,9 @@
+class CreateTeams < ActiveRecord::Migration[5.0]
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.integer :roster_size
+      t.integer :user_id
+    end
+  end
+end
---
+++ db/migrate/20170424013202_create_players.rb
@@ -0,0 +1,10 @@
+class CreatePlayers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :players do |t|
+      t.string :name
+      t.integer :number
+      t.string :position
+      t.integer :team_id
+    end
+  end
+end
