d687f77e443f4372098d9ea3588dd5046603185b
btcline04 <btcline04@gmail.com>
2017-04-26 18:54:52 -0400

    get and post players/new routes

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,5 +5,19 @@ class PlayersController < ApplicationController
     @players = Player.all 
     erb :'players/index'
   end
+
+  get '/players/new' do 
+    redirect_if_not_logged_in
+    erb :'players/new'
+  end
+
+  post '/players/new' do
+    if params[:name].empty? || params[:number].empty? || params[:position].empty?
+      redirect to "/players/new"
+    else
+      @player = Player.create(params)
+      redirect to "/players"
+    end
+  end
   
 end
