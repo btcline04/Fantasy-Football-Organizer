c481a0a12951b5e91ebf69aed889e9ac02c0f41b
btcline04 <btcline04@gmail.com>
2017-04-25 20:19:05 -0400

    added signup functionality to users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,12 +15,6 @@ class ApplicationController < Sinatra::Base
   end
 
   helpers do
-    def redirect_if_not_logged_in
-      if !logged_in?
-        redirect "/login?error=You have to be logged in to do that"
-      end
-    end
-
     def logged_in?
       !!session[:user_id]
     end
@@ -28,7 +22,6 @@ class ApplicationController < Sinatra::Base
     def current_user
       User.find(session[:user_id])
     end
-
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+
+  get '/signup' do
+    if !session[:user_id]
+      erb :'users/new'
+    else
+      redirect "/"
+    end
+  end
 end
