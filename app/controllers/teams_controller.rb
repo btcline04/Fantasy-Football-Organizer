afc902cfe6b8b56c816307c2f226cc250477f762
btcline04 <btcline04@gmail.com>
2017-04-26 14:50:48 -0400

    edit and show teams functionality

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -20,12 +20,30 @@ class TeamsController < ApplicationController
     end
   end
 
+  get '/teams/:id' do 
+    redirect_if_not_logged_in
+    @team = Team.find_by_id(params[:id])
+    erb :'teams/show'
+  end
+
   get '/teams/:id/edit' do
     redirect_if_not_logged_in
     @team = Team.find(params[:id])
     erb :'teams/edit'
   end
 
-  post '/teams/:id/edit' do 
+  post '/teams/:id' do 
+    if params[:name].empty? || params[:roster_size].empty?
+      redirect to "/teams/#{params[:id]}/edit"
+    else
+      @team = Team.find_by_id(params[:id])
+      @team.name = params[:name]
+      @team.roster_size = params[:roster_size]
+      @team.save
+      redirect to "/teams/#{@team.id}"
+    end
+  end
+
+
 
 end
