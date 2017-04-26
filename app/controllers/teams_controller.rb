88c8e35df2a22e1b149e6ae9818c643c8e3b6ac3
btcline04 <btcline04@gmail.com>
2017-04-25 20:32:59 -0400

    /teams route functionality

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,13 @@
 class TeamsController < ApplicationController
+
+  get '/teams' do
+    if logged_in?
+      @teams = Teams.all
+      erb :'teams/index'
+    else
+      redirect "/login"
+    end
+  end
+
+  
 end
