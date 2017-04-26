4d59d565e13c3160ef4ee539e749aa97ed2793ee
btcline04 <btcline04@gmail.com>
2017-04-25 20:28:00 -0400

    add signup features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,15 @@ class UsersController < ApplicationController
       redirect "/"
     end
   end
+
+  post '/signup' do
+    if params[:username].empty? || params[:password].empty?
+      redirect to "/signup"
+    else
+      binding.pry
+      @user = User.create(params)
+      @user.save
+      session[:id] = @user.id
+    end 
+  end
 end
