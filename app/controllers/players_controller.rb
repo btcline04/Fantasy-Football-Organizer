10927a6785d89059ad6de78960f176fae34ae80f
btcline04 <btcline04@gmail.com>
2017-04-26 21:26:07 -0400

    add checkbox to new player form to allow a player to be assigned to a team at creation

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -16,6 +16,7 @@ class PlayersController < ApplicationController
       redirect to "/players/new"
     else
       @player = Player.create(params)
+      binding.pry
       redirect to "/players"
     end
   end
@@ -29,17 +30,23 @@ class PlayersController < ApplicationController
   get '/players/:id/edit' do 
     redirect_if_not_logged_in
     @player = Player.find_by_id(params[:id])
-    erb: 'players/edit'
+    erb :'players/edit'
   end
 
   post '/players/:id' do
     if params[:name].empty? || params[:number].empty? || params[:position].empty?
       redirect to "/players/#{params[:id]}/edit"
     else
-      @player = Player.find_by_id(params[:id])
+      @player = Player.find(params[:id])
       @player.name = params[:name]
       @player.number = params[:number]
       @player.position = params[:position]
       @player.save
       redirect to "/players/#{@player.id}"
+    end
+  end
+
 end
+
+
+
